e97636d44137df70b686142e1f12ab80e21e315b
HeLLo2 <popt0@naver.com>
2025-03-16 16:09:52 +0900

    feat: [siddedish] ui 구현 및 동작 완성

--- sidedish/crawler.py
+++ sidedish/crawler.py
@@ -1,13 +1,16 @@
 import os
 from bs4 import BeautifulSoup
+import cloudscraper
 import requests;
+import time
 
 folder_path = "results"
-file_name = "list.html"
-file_path = os.path.join(folder_path, file_name)
+# file_path = os.path.join(folder_path, file_name)
+
+proxy_url = "54.248.238.110:80"
 
 # 목록 스캔
-def getListHtml(url, saveFile:bool = False):
+def getListHtml(url, headers, saveFile:bool = False):
     response = requests.get(url, headers=headers)
 
     if response.ok:
@@ -17,15 +20,16 @@ def getListHtml(url, saveFile:bool = False):
         os.makedirs(folder_path, exist_ok=True)
         result = soup.prettify()
         if saveFile:
-            with open(file_path, "w") as file:
+            list_path = os.path.join(folder_path, "list.html")
+            with open(list_path, "w") as file:
                 file.write(result)
-            print(f"File created at: {file_path}")
+            print(f"File created at: {list_path}")
 
         print("List crawling completed ✅")
         return result
 
     else:
-        print(response.text)
+        print(f"List crawling failed ❌: {response.status_code} ")
 
 # 파일 읽기
 def readFile(path):
@@ -44,13 +48,17 @@ class Chapter:
     def __iter__(self):
         return iter((self.title, self.link))
 
+# todo: novel_path가 global인 건 좋지 않음.
+novel_path: str
 # list html을 파싱해서 링크와 제목을 빈환.
 def parseList(listHtml):
-    soup = listHtml if (isinstance(listHtml, BeautifulSoup)) else soup = BeautifulSoup(listHtml, "html.parser")
+    soup = listHtml if isinstance(listHtml, BeautifulSoup) else BeautifulSoup(listHtml, "html.parser")
 
     # 노벨 제목
     title = soup.select_one(".view-title .view-content span b").get_text(strip=True)
-    os.makedirs(file_path + title, exist_ok=True)
+    global novel_path
+    novel_path = os.path.join(folder_path, title)
+    os.makedirs(novel_path, exist_ok=True)
     
     subject_a_tags = soup.select(".wr-subject a")
     chapter_list: list[Chapter] = []
@@ -67,26 +75,41 @@ def scanCapter(ch: Chapter, headers):
     response = requests.get(link, headers=headers)
     if response.ok:
         soup = BeautifulSoup(response.text, "html.parser")
-        # with open("results/" + title + ".txt", "w") as file:
-        #     file.write(soup.select_one('#novel_content').get_text().strip())
-        print(f"complete: {title}" )
+        with open(f"{os.path.join(novel_path, title)}.txt", "w") as file:
+            novel_soup = soup.select_one('#novel_content')
+            for a in novel_soup.find_all("p"):
+                a.insert_after("\n")
+                a.insert_after("\n")
+                a.unwrap()
+            file.write(novel_soup.getText().strip())
+        print(f"{title} ✅" )
 
     else:
-        print(response.status_code)
+        print(f"Chapter crawling failed ❌: title:{title} | status code: {response.status_code} ")
+
+def scanAllChapter(chapter_list: list[Chapter], headers, time_wait = 5, waitFunction: callable = None):
+        for ch in chapter_list:
+            scanCapter(ch, headers=headers)
+            if waitFunction:
+                waitFunction()
+            else:
+                time.sleep(time_wait)
+            
+        print("All chapter crawling completed 🚀")
+
+
+# Chapter 스캔 테스트용
+def scanCapterTest(ch: Chapter):
+    title, link = ch
+
+    file = readFile("TestChapter.html")
     
-
-PHPSESSID = "o10rgri979mfp81vvas56e7kge7dbing7m3m7vv5fvlsjsu0l8ke9uho99ikm0gf"
-el = "bm92ZWw%3D; HstCfa4678477=1741875018404"
-headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6,ja;q=0.5",
-    "Cookie": "PHPSESSID=vanc8rvknnmoc7tb53c0a9f6qi5vs2i1i2m89vuitd0vv6jac8hd8k31jehag13i; HstCfa4678477=1741783292412; HstCmu4678477=1741783292412; c_ref_4678477=https%3A%2F%2Fwww.xn--h10bx0wsvp.net%2F; e1192aefb64683cc97abb83c71057733=bm92ZWw%3D; HstCnv4678477=4; HstCns4678477=8; cf_clearance=yjP86XyEj8f8yBlCr2U8VPPi052g0EQv2iOiN2hDWY0-1741957326-1.2.1.1-vBpfFVL21sjFCGs0VcvUW2zX24Fr6Mv1BN9SR3Cq.Q4NSw2Ym4cjS43USgwUe7r7UmoFSlpQEGp61H0dCI6sDV.fBU8rgD183oxu4fqL_urL_qzL.kGa0vvzXb.hwaqMBcoMVArGJElQu3L9xGAx_oj4fLPJGFvlBkECvs4zgmcJk59C_sDMYV9QjQn.ab3._JTzlWfHqUmTIQSYLx9Gz_0n3BnW3A1L_Ukdbec2HedCxBG22FTiF6UHP_ZJ037idWF0ZHZKN.0IEqUdnqX6Ggb3exp5_p1dm20rRRqnSiDjXerAWx0z2uVLjXcpBSlzXMYjcAOGYLyBwDXY1d.Up6NZ64R1bhQE07.t3PfSdf3RClB88_vF0PUdTySTm31rVCKNGddN6d63k4_WTnh_lhn7OJ0kjtQOmLd1q4a3rCw; HstCla4678477=1741957329938; HstPn4678477=2; HstPt4678477=41",
-    "Referer": "https://booktoki468.com/bbs/captcha.php?bo_table=novel&wr_id=18081374&book=%EC%9D%BC%EB%B0%98%EC%86%8C%EC%84%A4",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
-}
-
-
-listHtml = readFile(file_path)
-chapter_list = parseList(listHtml)
-scanCapter(chapter_list[0], headers)
-
+    soup = BeautifulSoup(file, "html.parser")
+    with open(f"{os.path.join("results/", title)}.txt", "w") as file:
+        novel_soup = soup.select_one('#novel_content')
+        for a in novel_soup.find_all("p"):
+            a.insert_after("\n")
+            a.insert_after("\n")
+            a.unwrap()
+        file.write(novel_soup.getText().strip())
+    print(f"{title} ✅" )
--- sidedish/main.py
+++ sidedish/main.py
@@ -1,14 +1,4 @@
-import requests
-import os
-from bs4 import BeautifulSoup
+import ui
 
-url = "https://booktoki468.com/novel/17799638?book=%EC%9D%BC%EB%B0%98%EC%86%8C%EC%84%A4"
-headers = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "cookie": "PHPSESSID=vanc8rvknnmoc7tb53c0a9f6qi5vs2i1i2m89vuitd0vv6jac8hd8k31jehag13i; HstCfa4678477=1741783292412; HstCmu4678477=1741783292412; HstCnv4678477=1; c_ref_4678477=https%3A%2F%2Fwww.xn--h10bx0wsvp.net%2F; HstCns4678477=2; cf_clearance=QSomgLGuNWmgM4mO_Uo2qp8SyOcffIYyUo0HpUkGr74-1741785288-1.2.1.1-gf55ymGC2AWqoGjVkmfqAKBWnev6g8Mjr12I7dCQjQZme9uQhkJXPhTCzqllWSeKOTbkHZlVGaOpCX4RFAvige5fRTZQ8LJn.LGi_UPgWn_q42JNgSlbarKsdfGIYDH94s5rQeqV7PVQuI4qv9NfU_BEktXcgFqyqFn9iCUF8p6BHlKPmstmP.DIH00O2Ok8gtmUl9k.4JTVAFYpJ9zcotANp_veCQfA7z.p4bw.xfnQA63ZwITDeBGJ.8xnxA8etvZk8aSsL8PO3jkomifd4LbP6M9SW7fyM1dlUYtHh1RJip16bZrNvFWS4M4Hx_LmfSYRcm8ur3hnWbsMTMQXlo4b19d3aTvz45dxXUkNNbmBgfCyxNxJ5uzqnuY1inAYrv1LemuTdu_3N403tyC2Gs7YagA.UXmswT4.jL8L1DY; e1192aefb64683cc97abb83c71057733=bm92ZWw%3D; HstCla4678477=1741785505010; HstPn4678477=29; HstPt4678477=29",
-    "if-modified-since": "Wed, 12 Mar 2025 13:16:47 GMT",
-    "priority": "u=0, i",
-    "referer": "https://booktoki468.com/bbs/captcha.php?bo_table=novel&wr_id=17799638&book=%EC%9D%BC%EB%B0%98%EC%86%8C%EC%84%A4",
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
-}
-
+if __name__ == "__main__":
+    ui.start_ui()
